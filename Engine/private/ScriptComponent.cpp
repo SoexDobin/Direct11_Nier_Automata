3daed1ed4c96e05c562d9ad51cabc2ed7a5a7f59
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2026-02-27 13:02:07 +0900

    chore : final function

--- Engine/private/ScriptComponent.cpp
+++ Engine/private/ScriptComponent.cpp
@@ -1,5 +1,9 @@
 #include "ScriptComponent.h"
 
+ScriptComponent::ScriptComponent()
+{
+}
+
 ScriptComponent::ScriptComponent(const ComPtr<ID3D11Device>& device, const ComPtr<ID3D11DeviceContext>& context)
 	: Component(device, context)
 {
